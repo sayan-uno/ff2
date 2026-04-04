1e9975292bf7cb28ad32a5b8c5214b9d0e1099d3
sayan2nd <sm1555524@egmail.com>
2026-04-04 12:24:39 +0000

    change the upi id to "sayanmondalqr.payu@indus".

--- src/components/purchase-modal.tsx
+++ src/components/purchase-modal.tsx
@@ -371,7 +371,7 @@ export default function PurchaseModal({ product, user: initialUser, onClose }: P
             </>
         );
     case 'qrPayment':
-        const upiId = "sayanmo34536456u@indus";
+        const upiId = "sayanmondalqr.payu@indus";
         const upiUrl = `upi://pay?pa=${upiId}&pn=Garena&am=${finalPrice}&cu=INR&tn=${product.name}`;
         const minutes = Math.floor(qrCountdown / 60);
         const seconds = qrCountdown % 60;
